a7eb7174f0ad1fdd87c02e5fa146b9f984ab9461
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-17 06:42:19 +0200

    production v4

--- dist/index.js
+++ dist/index.js
@@ -13,9 +13,7 @@ const host = 'https://ddfc-89-209-185-240.ngrok-free.app';
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.static("public"));
-app.use(telegram_1.default.webhookCallback('secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg', {
-    secretToken: '6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg',
-}));
+app.use(telegram_1.default.webhookCallback('secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg'));
 // bot.telegram.setWebhook('https://ddfc-89-209-185-240.ngrok-free.app/secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg')
 telegram_1.default.telegram.setWebhook('https://bot-express-vercel-kik-80.vercel.app/secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg');
 app.use('/api', router_1.route);
