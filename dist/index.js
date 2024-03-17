0f277e44cb1e9e597db1c39edd045c195149a316
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-17 07:16:37 +0200

    production v7

--- dist/index.js
+++ dist/index.js
@@ -13,9 +13,9 @@ const host = 'https://ddfc-89-209-185-240.ngrok-free.app';
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.static("public"));
-app.use(telegram_1.default.webhookCallback('secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg'));
+// app.use(bot.webhookCallback('secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg'));
 // bot.telegram.setWebhook('https://ddfc-89-209-185-240.ngrok-free.app/secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg')
-telegram_1.default.telegram.setWebhook('https://bot-express-vercel-kik-80.vercel.app/secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg');
+// bot.telegram.setWebhook('https://bot-express-vercel-kik-80.vercel.app/secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg');
 app.use('/api', router_1.route);
 app.get('/', (_req, res) => {
     return res.send('Express Typescript on Vercel');
@@ -24,15 +24,14 @@ app.get('/ping', (_req, res) => {
     telegram_1.default.launch(() => console.log('Start bot'));
     return res.send('pong 🏓');
 });
-telegram_1.default.launch(() => console.log('Start bot'));
-// bot.launch({
-//   webhook: {
-//     domain: '17b1-89-209-84-19.ngrok-free.app',
-//     hookPath: '/secret-code/6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc',
-//   },
-// },
-// ()=>console.log('Start bot'),
-// );
+// bot.launch(()=>console.log('Start bot'))
+telegram_1.default.launch({
+    webhook: {
+        domain: 'bot-express-vercel-kik-80.vercel.app',
+        hookPath: '/secret-code/6884974307:AAEXO7AVmAtsoB4QHXjYuLPKqpPzs__VeSg',
+        port: 3000
+    },
+}, () => console.log('Start bot'));
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
